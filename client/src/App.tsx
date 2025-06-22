2635a74caa61c5865533ad086d5288239097908a
Alon Ganot <alon1ganot@gmail.com>
2025-06-22 11:59:11 +0300

    added navbar for notifications display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import { EventSender } from './Components/EventSender'
+import Navbar from './Components/Navbar'
+import { NotificationsProvider } from './context/NotificationsContext'
 
 function App() {
-
   return (
-    <>
-      <EventSender/>
-    </>
+    <NotificationsProvider>
+      <Navbar />
+      <div style={{ paddingTop: '3.5rem' }}>
+        <EventSender />
+      </div>
+    </NotificationsProvider>
   )
 }
 
