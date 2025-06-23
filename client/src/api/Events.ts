d7eac6610a831e8ea942291035114fc5350a1241
אריאל סיצ'יוב <sychovar@gmail.com>
2025-06-23 16:31:40 +0300

    feat: use entities types in client

--- client/src/api/Events.ts
+++ client/src/api/Events.ts
@@ -1,15 +1,22 @@
 import { appConfig } from "../appConfig";
+import type { LineEvent } from '@Entities/LineEvent'
 
 export const sendEvent = async (event: string) => {
         const status = event === 'קו נכנס לתוקף' ? 'CURRENT' : event === 'קו יצא מתוקף' ? "EXPIRED" : "DELETED" // This should be smarter
-
+        const lineEvent: LineEvent = {
+            id: 'L123', 
+            status, 
+            userId: localStorage.getItem("user") ?? 'לא ידוע',
+            date: new Date
+        }
+        
         try {
             await fetch(`${appConfig.SERVER_URL}/events/update`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ id: 'L123', status, userId: localStorage.getItem("user") }),
+                body: JSON.stringify(lineEvent),
             });
         } catch (error: any) {
             console.log(`Error: ${error.message}`);
